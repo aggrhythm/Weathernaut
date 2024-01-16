b8f8df3a85a9ca269ffe5da75877125b23c50a59
Rhythm Aggarwal <aggarwalrhythm2003@gmail.com>
2024-01-16 22:15:52 +0530

    icons changed

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -53,7 +53,7 @@ const WeatherCard=({
         <p className='flex-1 text-center p-2'>{time}</p>
       </div>
       <div className='w-full flex justify-between items-center mt-4 gap-4'>
-        <p className='flex-1 text-center p-2 font-bold bg-blue-600 shadow rounded-lg'>Wind Speed <p className='font-normal'>{windspeed} km/h</p></p>
+        <p className='flex-1 text-center p-2 font-bold bg-green-600 shadow rounded-lg'>Wind Speed <p className='font-normal'>{windspeed} km/h</p></p>
         <p className='flex-1 text-center p-2 font-bold rounded-lg bg-green-600'>Humidity <p className='font-normal'>{humidity} gm/m&#179;</p></p>
       </div>
       <div className='w-full p-3 mt-4 flex justify-between items-center'>
