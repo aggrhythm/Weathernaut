1bad5d3702d225a3e4f4b52f23dcb10a863d4ba5
Rhythm Aggarwal <aggarwalrhythm2003@gmail.com>
2024-01-16 23:58:18 +0530

    done

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -61,7 +61,7 @@ const WeatherCard=({
         <p className='text-lg'>{heatIndex ? heatIndex : 'N/A'}</p>
       </div>
       <hr className='bg-slate-600' />
-      <div className='w-full p-4 flex justify-center items-center text-3xl font-semibold'>
+      <div className='w-full p-4 flex justify-center items-center text-2xl font-semibold'>
         {conditions}
       </div>
     </div>
