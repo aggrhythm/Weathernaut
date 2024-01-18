d28e669ea252c649a2d18aa605ac9626268cff41
Rhythm Aggarwal <aggarwalrhythm2003@gmail.com>
2024-01-18 16:52:20 +0530

    bg image

--- src/components/BackgroundLayout.jsx
+++ src/components/BackgroundLayout.jsx
@@ -36,7 +36,7 @@ const BackgroundLayout = () => {
     }
   },[weather])
   return (
-    <img src={image} alt="weather_image" className='h-screen w-full fixed left-0 top-0 -z-[10]'/>
+    <img src={image} alt="weather_image" className='h-screen w-full fixed left-0 top-0 -z-[20] bg-opacity-25'/>
   )
 }
 
